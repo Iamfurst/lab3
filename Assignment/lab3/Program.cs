2e0c65388f33cc5a8f2629036a7fb0efde5b961f
Prince Onuoha <princeonuohja3@gmail.com>
2022-05-11 22:13:55 +0100

    lab

---
+++ Assignment/lab3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("This is a calculator app");
+             Console.Write("Type in your Prime number: ");
+             int PrimeNumber = int.Parse(Console.ReadLine());
+             Console.WriteLine("The value you entered is a Prime number");
+             Console.Write("Type in your second number: ");
+             int secondNumber = int.Parse(Console.ReadLine());
+             Console.WriteLine("The value you entered is a not Prime number");
+        }
+    }
+}
